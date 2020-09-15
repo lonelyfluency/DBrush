6d2aa641ecac1c8a2ae9da98bf0b569b733f81c8
lonelyfluency <lonelyfluency@gmail.com>
2020-09-15 14:20:07 +0800

    canvas created

---
+++ canvas.py
@@ -0,0 +1,21 @@
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+plt.axis([-50,50,-50,50])
+
+
+ax = plt.gca()
+
+X = np.random.randint(-50,50,20).tolist()
+Y = np.random.randint(-50,50,20).tolist()
+
+for i in range(20):
+    
+    ax.scatter(X[i],Y[i],s=10,c='k',marker='.')
+    ax.scatter(Y[i],X[i],s=30,c='k',marker='.')
+    plt.pause(0.01)
+
+plt.show()
